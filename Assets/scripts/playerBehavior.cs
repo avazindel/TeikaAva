eecefa007632f3e79ee3d33426ace3af4ae6a7de
avazindel <ava.zindel@gmail.com>
2026-01-29 14:42:12 -0600

    droppable fruit
    adjusted fruit gravity, movement, and space between fruit and player

--- Assets/scripts/playerBehavior.cs
+++ Assets/scripts/playerBehavior.cs
@@ -5,6 +5,9 @@ public class playerBehavior : MonoBehaviour
 {
 
     public float speed;
+    public GameObject fruit;
+
+    public float offY = -0.8f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +21,29 @@ public class playerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (fruit != null)
+        {
+            Vector3 playerPos = transform.position;
+            Vector3 fruitOffset = new Vector3(0.0f, offY, 0.0f);
+                fruit.transform.position = playerPos + fruitOffset;
+        }
+
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Rigidbody2D body = fruit.GetComponent<Rigidbody2D>();
+            body.gravityScale = 1.0f;
+
+            Collider2D collider = fruit.GetComponent<Collider2D>();
+            collider.enabled = true;
+
+            fruit = null;
+
+        }
+
+
+
         if (Keyboard.current.leftArrowKey.isPressed)
         {
             Vector3 newPos = transform.position;
